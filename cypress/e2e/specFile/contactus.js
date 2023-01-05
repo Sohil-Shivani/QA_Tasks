ab9f1d54c82715e557783995f1ee48ec014d868d
Sohil <sohil.shivani@gmail.com>
2023-01-05 10:50:57 +0530

    test

---
+++ cypress/e2e/specFile/contactus.js
@@ -0,0 +1,53 @@
+///reference type = 'cypress'
+import ContactUsPage from "../pageObectFile/pageObjects"
+
+
+describe('Contact Us page functionality', () => {
+    let testData
+    beforeEach(() => {
+        cy.visit(Cypress.env('url'))
+        cy.fixture('example').then(function (dataJson) {
+            testData = dataJson
+            return testData
+        })
+
+    })
+    // before(() => {
+    //     cy.fixture('example').then(function (dataJson) {
+    //         testData = dataJson
+    //         return testData
+    //     })
+    // })
+    context('Contact US page functionality', () => {
+        const contactUsObject = new ContactUsPage()
+
+        it('Validate user landed on Contact Us page.', () => {
+            contactUsObject.navigateToContactUsPage()
+        })
+        it('Positive Case : Validate Contact Us page.', () => {
+            contactUsObject.navigateToContactUsPage()
+            contactUsObject.getFirstName()
+            contactUsObject.getLastName()
+            contactUsObject.getEmail()
+            contactUsObject.getMobilePhone()
+            contactUsObject.selectDropDown()
+            contactUsObject.writeMessage(testData.desmsg)
+            contactUsObject.clickOnSubmit()
+            //contactUsObject.validateSuccessMsg()
+        })
+        it('Negative Case : Validate Contact Us page', () => {
+            const msg = " "
+            contactUsObject.navigateToContactUsPage()
+            contactUsObject.getFirstName()
+            contactUsObject.getLastName()
+            contactUsObject.getEmail()
+            contactUsObject.getMobilePhone()
+            contactUsObject.selectDropDown()
+            contactUsObject.writeMessage(msg)
+            contactUsObject.clickOnSubmit()
+            contactUsObject.validateMandatoryField()
+        })
+
+    })
+})
+
